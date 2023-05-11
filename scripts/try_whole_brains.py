098f75bfa6575d28a50e71339080a242933f84ca
bdpedigo <benjamindpedigo@gmail.com>
2023-05-11 17:31:01 -0400

    bump

--- scripts/try_whole_brains.py
+++ scripts/try_whole_brains.py
@@ -41,74 +41,41 @@ def gluefig(name, fig, **kwargs):
 
 # %%
 
-
-def load_nblast():
-    data_dir = DATA_PATH / "hackathon"
-
-    nblast = pl.scan_ipc(
-        data_dir / "nblast" / "nblast_flywire_mcns_comp.feather",
-        memory_map=False,
-    )
-    index = pd.Index(nblast.select("index").collect().to_pandas()["index"])
-    columns = pd.Index(nblast.columns[1:])
-
-    if index.dtype == "object":
-        index_ids = index.str.split(",", expand=True).get_level_values(0).astype(int)
-    else:
-        index_ids = index
-    column_ids = columns.str.split(",", expand=True).get_level_values(0).astype(int)
-
-    print(column_ids)
-
-    index_ids_map = dict(zip(index_ids, index))
-    column_ids_map = dict(zip(column_ids, columns))
-    index_ids_reverse_map = dict(zip(index, index_ids))
-    column_ids_reverse_map = dict(zip(columns, column_ids))
-
-    query_index = pd.Series([index_ids_map[i] for i in index])
-    query_columns = pd.Series(["index"] + [column_ids_map[i] for i in columns])
-
-    nblast = nblast.with_columns(
-        pl.col("index").is_in(query_index).alias("select_index")
-    )
-
-    mini_nblast = (
-        nblast.filter(pl.col("select_index"))
-        .select(query_columns)
-        .collect()
-        .to_pandas()
-    ).set_index("index")
-
-    mini_nblast.index = mini_nblast.index.map(index_ids_reverse_map)
-    mini_nblast.columns = mini_nblast.columns.map(column_ids_reverse_map)
-
-    mini_nblast = mini_nblast.loc[query_node_ids, query_node_ids]
-
-    return mini_nblast
-
-
 currtime = time.time()
 # nblast = load_nblast()
+
 nblast = pd.read_feather(
     data_dir / "nblast" / "nblast" / "nblast_flywire_mcns_comp.feather",
 )
 print(
     f"{time.time() - currtime:.3f} seconds elapsed to load FlyWire vs. MaleCNS NBLASTs."
 )
-quit()
+
 
 # %%
+
+n_left = nblast.shape[0]
+n_right = nblast.shape[1]
+
 a = np.ones(n_left)
 b = np.ones(n_right) / n_right * n_left
-nblast_between = nblast.loc[left_nodes.index, right_nodes.index]
-S = nblast_between.values.astype(float)
-
+S = nblast.values.astype(float)
 
 currtime = time.time()
 sinkhorn_sol, log = ot.sinkhorn(
-    a, b, -S, reg=0.03, numItermax=2000, verbose=True, log=True
+    a, b, -S, reg=0.03, numItermax=200, verbose=True, log=True
 )
-print(f"{time.time() - currtime:.3f} seconds elapsed.")
+print(f"{time.time() - currtime:.3f} seconds elapsed to sinkhorn.")
+
+currtime = time.time()
+threshold = 1e-3
+sinkhorn_sol[sinkhorn_sol < threshold] = 0
+print(f"{time.time() - currtime:.3f} seconds elapsed to threshold.")
+
+currtime = time.time()
+sinkhorn_sol = pd.DataFrame(sinkhorn_sol, index=nblast.index, columns=nblast.columns)
+sinkhorn_sol.to_feather(data_dir / "nblast" / "nblast" / "sinkhorn_sol.feather")
+print(f"{time.time() - currtime:.3f} seconds elapsed to save.")
 
 # %%
 fig, ax = plt.subplots(1, 1, figsize=(5, 5))
